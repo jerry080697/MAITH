4fa8e96f267e3b4a6de549a310f6501142859475
김병준 <162072124+jerry080697@users.noreply.github.com>
2025-06-15 14:12:44 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CLIENT_ID: '1024506320862-qvo22o2c1233uqhcgh0kairei00fkeeo.apps.googleusercontent.com',
-    CLIENT_SECRET: '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',
+    CLIENT_ID: 'SECRET',
+    CLIENT_SECRET: 'SECRET',
   }
 };
 
